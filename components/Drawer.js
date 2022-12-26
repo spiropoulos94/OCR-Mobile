0bfe01138aa6f56460886d3aaccdb30d9da986b1
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-12-26 17:47:46 +0200

    test

--- components/Drawer.js
+++ components/Drawer.js
@@ -31,36 +31,90 @@ const styles = StyleSheet.create({
   },
 });
 
-const Drawer = ({status, setStatus, children, title, fullheight}) => {
+const FullHeightModal = ({title, status, setStatus, children}) => {
   return (
     <Modal
-      transparent={fullheight ? false : true}
-      animationType={fullheight ? 'slide' : ''}
-      presentationStyle={fullheight ? 'pageSheet' : ''}
       visible={status}
+      animationType="slide"
+      presentationStyle="pageSheet"
       onRequestClose={() => {
         setStatus(false);
       }}>
-      <View
-        style={{
-          backgroundColor: fullheight ? '' : 'rgba(0,0,0,0.5)',
-          height: '100%',
-        }}>
-        <View style={[styles.drawer, fullheight ? '' : styles.customHeight]}>
-          <View style={styles.header}>
-            <TouchableOpacity onPress={() => setStatus(false)}>
-              <Icon name="chevron-down" size={20} />
-            </TouchableOpacity>
-            <Text> {title} </Text>
-            <TouchableOpacity>
-              <Text> Reset </Text>
-            </TouchableOpacity>
-            {/* <Icon name="filter" size={20} /> */}
-          </View>
-          {children}
-        </View>
-      </View>
+      <ModalHeader title={title} setStatus={setStatus} />
+      {children}
     </Modal>
+  );
+};
+
+const CustomHeightModal = ({title, status, setStatus, children}) => {
+  return (
+    <Modal
+      visible={status}
+      animationType="slide"
+      presentationStyle="pageSheet"
+      onRequestClose={() => {
+        setStatus(false);
+      }}>
+      <ModalHeader title={title} setStatus={setStatus} /> {children}
+    </Modal>
+  );
+};
+
+const ModalHeader = ({title, setStatus}) => {
+  return (
+    <View style={styles.header}>
+      <TouchableOpacity onPress={() => setStatus(false)}>
+        <Icon name="chevron-down" size={20} />
+      </TouchableOpacity>
+      <Text> {title} </Text>
+      <TouchableOpacity>
+        <Text> Reset </Text>
+      </TouchableOpacity>
+      {/* <Icon name="filter" size={20} /> */}
+    </View>
+  );
+};
+
+const Drawer = ({status, setStatus, children, title, fullheight}) => {
+  return (
+    // <Modal
+    //   transparent={fullheight ? false : true}
+    //   animationType={fullheight ? 'slide' : ''}
+    //   presentationStyle={fullheight ? 'pageSheet' : ''}
+    //   visible={status}
+    //   onRequestClose={() => {
+    //     setStatus(false);
+    //   }}>
+    //   <View
+    //     style={{
+    //       backgroundColor: fullheight ? '' : 'rgba(0,0,0,0.5)',
+    //       height: '100%',
+    //     }}>
+    //     <View style={[styles.drawer, fullheight ? '' : styles.customHeight]}>
+    //       <ModalHeader title={title} setStatus={setStatus} />
+    //       {children}
+    //     </View>
+    //   </View>
+    // </Modal>
+    <>
+      {fullheight ? (
+        <FullHeightModal
+          status={status}
+          setStatus={setStatus}
+          children={children}
+          title={title}>
+          {children}
+        </FullHeightModal>
+      ) : (
+        <CustomHeightModal
+          status={status}
+          setStatus={setStatus}
+          children={children}
+          title={title}>
+          {children} >{children}
+        </CustomHeightModal>
+      )}
+    </>
   );
 };
 
