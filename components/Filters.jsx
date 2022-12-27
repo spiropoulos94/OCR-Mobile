ccd062ae00d7ae3ffe4802bc0c87c2e6125f7616
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-12-27 17:07:59 +0200

    added react native elements library

--- components/Filters.jsx
+++ components/Filters.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text} from 'react-native';
+import {ButtonGroup} from '@rneui/themed';
 
 const Filters = () => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
   return (
     <View>
       <Text>WE ARE THE FILTERS</Text>
+      <ButtonGroup
+        buttons={['SIMPLE', 'BUTTON', 'GROUP']}
+        selectedIndex={selectedIndex}
+        onPress={value => {
+          setSelectedIndex(value);
+        }}
+        containerStyle={{marginBottom: 20}}
+      />
     </View>
   );
 };
