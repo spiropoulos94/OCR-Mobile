9ad87cf6e2e40264cd6d752ab4406b2481f1a003
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-12-27 23:48:40 +0200

    added products to product context

--- components/Filters.jsx
+++ components/Filters.jsx
@@ -1,6 +1,8 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import {View, Text} from 'react-native';
 import {ButtonGroup, Button} from '@rneui/themed';
+// contexts
+import ProductsContext from './../contexts/products.context';
 
 const Filters = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
@@ -16,7 +18,12 @@ const Filters = () => {
         }}
         containerStyle={{marginBottom: 20}}
       />
-      <Button>Apply</Button>
+      <Button
+        onPress={() => {
+          alert(1);
+        }}>
+        Apply
+      </Button>
     </View>
   );
 };
--- contexts/products.context.js
+++ contexts/products.context.js
@@ -5,12 +5,32 @@ import {createContext, useState} from 'react';
 export const ProductsContext = createContext({
   products: [],
   setProducts: () => null,
+  filteredProducts: [],
+  setFilteredProducts: () => null,
 });
 
 export const ProductsProvider = ({children}) => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([
+    {
+      name: 'Milk',
+      expDateTs: 1671317840973,
+      addedOn: 1671317851535,
+    },
+    {
+      name: 'Cheese',
+      expDateTs: 1671317840973,
+      addedOn: 1671317851535,
+    },
+    {
+      name: 'Yogurt',
+      expDateTs: 1671317840973,
+      addedOn: 1671317851535,
+    },
+  ]);
 
-  const value = {products, setProducts};
+  const [filteredProducts, setFilteredProducts] = useState(null);
+
+  const value = {products, setProducts, filteredProducts, setFilteredProducts};
 
   return (
     <ProductsContext.Provider value={value}>
