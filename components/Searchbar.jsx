2ac0a23a0a339ad3733090ce1188e7ce374c9bc0
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-12-30 01:00:15 +0200

    search bar init

--- components/Searchbar.jsx
+++ components/Searchbar.jsx
@@ -1,9 +1,19 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import {StyleSheet, Text, View, TextInput, Keyboard} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+import {ProductsContext} from '../contexts/products.context';
+
 const Searchbar = ({style}) => {
   const [input, setInput] = useState('');
+
+  const {setFilteredProducts} = useContext(ProductsContext);
+
+  useEffect(() => {
+    if (input.length) {
+      setFilteredProducts([]);
+    }
+  }, [input]);
 
   return (
     <View style={[styles.searchBarWrapper, style]}>
