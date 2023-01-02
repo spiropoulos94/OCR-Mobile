ddc9f592240bcac563ec48264412c92eb7c453d9
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2023-01-02 14:24:15 +0200

    init filter ui

--- components/Filters.jsx
+++ components/Filters.jsx
@@ -1,13 +1,48 @@
 import React, {useState, useContext} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 // contexts
 import {ProductsContext} from '../contexts/products.context';
 import CustomButton from './Button';
 
+const sortOptions = [
+  {
+    title: 'ExpDateAsc',
+    label: 'Expiry date soonest to latest',
+  },
+  {
+    title: 'ExpDateDesc',
+    label: 'Expiry date latest to soonest',
+  },
+  {
+    title: 'AddedDateAsc',
+    label: 'Added date most recent first',
+  },
+  {
+    title: 'AddedDateDesc',
+    label: 'Added date oldest first',
+  },
+];
+
+const Sort = () => {
+  return (
+    <View>
+      <Text>Sort By</Text>
+      {sortOptions.map(o => (
+        <View>
+          <Text>{o.label}</Text>
+        </View>
+      ))}
+    </View>
+  );
+};
+
 const Filters = ({setFilteredProducts, products}) => {
+  const [sortOrder, setSortOrder] = useState(null);
+
   return (
     <View>
       <Text>WE ARE THE FILTERS</Text>
+      <Sort setSortOrder={setSortOrder} />
       <CustomButton
         title={'APPLY'}
         clickFN={() => {
@@ -20,5 +55,7 @@ const Filters = ({setFilteredProducts, products}) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({});
 
 export default Filters;
