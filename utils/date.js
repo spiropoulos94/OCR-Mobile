9a6e8afca76028f312cd86edaab9d455ec3f885c
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2023-01-21 19:20:35 +0200

    added new parsing technique

--- utils/date.js
+++ utils/date.js
@@ -14,7 +14,7 @@ export function stringToTimestamp(dateString) {
 
 function extractTimestamp(dateString) {
   var dateRegex =
-    /(\d{2})([\/|\.|\-|#|0|1|2])(\d{2})([\/|\.|\-|#|0|1|2])(\d{4})/;
+    /(\d{2})([\/|\.|\-|#|0|1|2])(\d{2})([\/|\.|\-|#|0|1|2|3|4|5|6|7|8|9|0])(\d{4})/;
   var match = dateRegex.exec(dateString);
   if (match) {
     var day = match[1];
