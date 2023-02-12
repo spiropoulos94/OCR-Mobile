59b2f8bbbebf92ba1219066cffed84a98305e989
Nikos Spiropoulos <spiropoulos94@gmail.com>
2023-02-12 18:48:35 +0200

    added test screen for experiments

--- components/tabs/TestTab.jsx
+++ components/tabs/TestTab.jsx
@@ -4,10 +4,43 @@ import {pageStyles} from '../../styles/pageStyles';
 import ImagePicker from 'react-native-image-crop-picker';
 import ImageEditor from "@react-native-community/image-editor";
 import { Button } from '@rneui/base';
+import TextRecognition from '@react-native-ml-kit/text-recognition';
 
 const TestTabScreen = () => {
 
     const [img, setImg] = useState("")
+
+    const handleConvert = async () => {
+        console.log("Ready to parse imagec => ", img)
+        try {
+          const result = await TextRecognition.recognize(
+            img,
+          );
+          console.log({result})
+          let totalTexts = [];
+          for (let block of result.blocks) {
+            // totalText += ' ' + block.text;
+            totalTexts.push(block.text);
+            for (let line of block.lines) {
+              // totalText += '' + line.text;
+              totalTexts.push(line.text);
+            }
+          }
+    
+          console.log({totalTexts})
+    
+        //   if(totalTexts && totalTexts.length > 0){
+        //     let dates = extractDate(totalTexts);
+        //     if(dates && dates.length > 0){
+        //       dates.forEach(date => console.log(`Check! ${stringToTimestamp(date)}`));
+        //     }
+        //   }
+    
+        } catch (e) {
+          console.log(e.message);
+          alert('Error: Check console');
+        }
+      };
 
     const handlePress = async () => {
         await ImagePicker.openPicker({
@@ -38,6 +71,7 @@ const TestTabScreen = () => {
     <View style={pageStyles.container}>
       <Text>This is a test screen</Text>
       <Button onPress={handlePress} title={"Selectimage from library"} />
+      <Button onPress={handleConvert} title={"Convert"} />
       <Image
         style={{
           width: '100%',
