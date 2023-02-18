c927cc45d28bad1b84ae8ad1e82a0aad271a8a3f
Nikos Spiropoulos <spiropoulos94@gmail.com>
2023-02-18 18:46:11 +0200

    small refactor on calling api

--- components/tabs/CloudVisionTab.jsx
+++ components/tabs/CloudVisionTab.jsx
@@ -14,15 +14,45 @@ const getImageInBase64 = async () => {
       return base64
 }
 
+function generateBody(image) {
+    const body = {
+      requests: [
+        {
+          image: {
+            content: image,
+          },
+          features: [
+            {
+              type: 'TEXT_DETECTION', //we will use this API for text detection purposes.
+              maxResults: 1,
+            },
+          ],
+        },
+      ],
+    };
+    return body;
+  }
+
+async function callGoogleVisionAsync(image) {
+    const body = generateBody(image); //pass in our image for the payload
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    const result = await response.json();
+    console.log(result);
+  }
+
 
 const CloudVisionTab = () => {
 
-    console.log({CLOUD_VISION_API_KEY, API_KEY, API_URL})
-
     const handlePress = async () => {
-      let res = await getImageInBase64()
-      console.log({res})
-
+        let image = await getImageInBase64()
+        await callGoogleVisionAsync(image)
     }
       
     return (<View>
