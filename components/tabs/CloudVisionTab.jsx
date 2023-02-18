edb83b3073a47255ffa3d76bac950c73cbbd287e
Nikos Spiropoulos <spiropoulos94@gmail.com>
2023-02-18 02:52:26 +0200

    get image in base64

--- components/tabs/CloudVisionTab.jsx
+++ components/tabs/CloudVisionTab.jsx
@@ -1,13 +1,33 @@
 import {View, Text} from "react-native"
 import {CLOUD_VISION_API_KEY} from "@env"
+import { Button } from "@rneui/base"
+import ImagePicker from 'react-native-image-crop-picker';
+
+const getImageInBase64 = async () => {
+   let base64 = await ImagePicker.openPicker({
+        width: 150,
+        height: 100,
+        cropping: true,
+        includeBase64: true,
+      }).then(image => image.data);
+      
+      return base64
+}
 
 
 const CloudVisionTab = () => {
 
     console.log({CLOUD_VISION_API_KEY})
 
+    const handlePress = async () => {
+      let res = await getImageInBase64()
+      console.log({res})
+
+    }
+      
     return (<View>
         <Text>This is the cloud vision tab</Text>
+        <Button onPress={()=>handlePress()} title={'Library select'}></Button>
     </View>)
 }
 
