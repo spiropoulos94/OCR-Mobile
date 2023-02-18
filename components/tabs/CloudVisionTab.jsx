f9e2549378763ba4491e0cc8f0460c3b7185fd2b
Nikos Spiropoulos <spiropoulos94@gmail.com>
2023-02-18 02:09:45 +0200

    added .env

--- components/tabs/CloudVisionTab.jsx
+++ components/tabs/CloudVisionTab.jsx
@@ -1,7 +1,11 @@
 import {View, Text} from "react-native"
+import {CLOUD_VISION_API_KEY} from "@env"
 
 
 const CloudVisionTab = () => {
+
+    console.log({CLOUD_VISION_API_KEY})
+
     return (<View>
         <Text>This is the cloud vision tab</Text>
     </View>)
